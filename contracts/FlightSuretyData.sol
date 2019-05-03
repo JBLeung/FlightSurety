bbe6075e4d0aace82a920e74e06c582d7f972cdb
JBLeung <jbleunghk@gmail.com>
2019-05-03 15:28:52 +0800

    fix data contract lint issue

--- contracts/FlightSuretyData.sol
+++ contracts/FlightSuretyData.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.25;
 
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 
+
 contract FlightSuretyData {
     using SafeMath for uint256;
 
@@ -21,12 +22,16 @@ contract FlightSuretyData {
     * @dev Constructor
     *      The deploying account becomes contractOwner
     */
-    constructor
-                                (
-                                ) 
-                                public 
-    {
+    constructor() public {
         contractOwner = msg.sender;
+    }
+
+     /**
+    * @dev Fallback function for funding smart contract.
+    *
+    */
+    function() external payable {
+        fund();
     }
 
     /********************************************************************************************/
@@ -38,10 +43,10 @@ contract FlightSuretyData {
 
     /**
     * @dev Modifier that requires the "operational" boolean variable to be "true"
-    *      This is used on all state changing functions to pause the contract in 
+    *      This is used on all state changing functions to pause the contract in
     *      the event there is an issue that needs to be fixed
     */
-    modifier requireIsOperational() 
+    modifier requireIsOperational()
     {
         require(operational, "Contract is currently not operational");
         _;  // All modifiers require an "_" which indicates where the function body will be added
@@ -61,32 +66,51 @@ contract FlightSuretyData {
     /********************************************************************************************/
 
     /**
-    * @dev Get operating status of contract
-    *
-    * @return A bool that is the current operating status
-    */      
-    function isOperational() 
-                            public 
-                            view 
-                            returns(bool) 
-    {
-        return operational;
-    }
-
-
-    /**
     * @dev Sets contract operations on/off
     *
     * When operational mode is disabled, all write transactions except for this one will fail
-    */    
-    function setOperatingStatus
-                            (
-                                bool mode
-                            ) 
-                            external
-                            requireContractOwner 
-    {
+    */
+
+    function setOperatingStatus(bool mode) external requireContractOwner {
         operational = mode;
+    }
+
+    /**
+    * @dev Add an airline to the registration queue
+    *      Can only be called from FlightSuretyApp contract
+    *
+    */
+    function registerAirline() external pure {
+    }
+
+    /**
+    * @dev Buy insurance for a flight
+    *
+    */
+    function buy() external payable {
+    }
+
+    /**
+     *  @dev Credits payouts to insurees
+    */
+    function creditInsurees() external pure {
+    }
+
+    /**
+     *  @dev Transfers eligible payout funds to insuree
+     *
+    */
+    function pay() external pure {
+    }
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */
+
+    function isOperational() public view returns(bool) {
+        return operational;
     }
 
     /********************************************************************************************/
@@ -94,93 +118,15 @@ contract FlightSuretyData {
     /********************************************************************************************/
 
    /**
-    * @dev Add an airline to the registration queue
-    *      Can only be called from FlightSuretyApp contract
-    *
-    */   
-    function registerAirline
-                            (   
-                            )
-                            external
-                            pure
-    {
-    }
-
-
-   /**
-    * @dev Buy insurance for a flight
-    *
-    */   
-    function buy
-                            (                             
-                            )
-                            external
-                            payable
-    {
-
-    }
-
-    /**
-     *  @dev Credits payouts to insurees
-    */
-    function creditInsurees
-                                (
-                                )
-                                external
-                                pure
-    {
-    }
-    
-
-    /**
-     *  @dev Transfers eligible payout funds to insuree
-     *
-    */
-    function pay
-                            (
-                            )
-                            external
-                            pure
-    {
-    }
-
-   /**
     * @dev Initial funding for the insurance. Unless there are too many delayed flights
     *      resulting in insurance payouts, the contract should be self-sustaining
     *
-    */   
-    function fund
-                            (   
-                            )
-                            public
-                            payable
-    {
+    */
+    function fund() public payable {
     }
 
-    function getFlightKey
-                        (
-                            address airline,
-                            string memory flight,
-                            uint256 timestamp
-                        )
-                        pure
-                        internal
-                        returns(bytes32) 
-    {
+    function getFlightKey(address airline, string memory flight, uint256 timestamp) internal pure returns(bytes32) {
         return keccak256(abi.encodePacked(airline, flight, timestamp));
     }
-
-    /**
-    * @dev Fallback function for funding smart contract.
-    *
-    */
-    function() 
-                            external 
-                            payable 
-    {
-        fund();
-    }
-
-
 }
 
