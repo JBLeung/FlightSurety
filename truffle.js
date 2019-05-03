b267a73cfb5d91cc6940d4a08820360a344c4a35
JBLeung <jbleunghk@gmail.com>
2019-05-03 22:44:29 +0800

    update truffle config

--- truffle.js
+++ truffle.js
@@ -6,7 +6,6 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*',
-      gas: 9999999999999,
       gasPrice: 1,
       accounts: 50,
       defaultEtherBalance: 1000,
